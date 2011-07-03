67cc34cfb8df1d0e6ee715b6b88e218f010c4476
Mark Visser <mjmvisser@gmail.com>
2011-07-03 17:40:22 -0400

    removed version from long description

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ files = ["atitweak", "adl3/*"]
 
 setup(name="adl3",
       version="0.2",
-      description="ADL (AMD Display Library) 0.1",
+      description="ADL (AMD Display Library)",
       author="Mark Visser",
       author_email="mjmvisser@gmail.com",
       url="https://github.com/mjmvisser/adl3",
